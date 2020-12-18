4e8d1d8a2335ec39451df221be9876cf4c14a1e9
MatthewBosek <matt2.bosek@yahoo.com>
2020-12-17 19:39:03 -0500

    Udemy commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {Checkbox} from 'evergreen-ui';
+import React, {useState} from 'react';
 
 function App() {
+  const [v, setV] = useState(false);
+  
+
   return (
     <div className="App">
-      
+      <h1>Learn React Testing
+      </h1>
+      <input data-test="regular-checkbox"
+            name="isGoing"
+            type="checkbox"
+            checked={v}
+            onChange={({target: {checked}}) => setV(checked)} />
+      <Checkbox data-test="evergreen-ui-checkbox" checked={v}
+        onChange={({target: {checked}}) => setV(checked)}
+        />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,76 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react';
+import Enzyme, { shallow, mount } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { Checkbox } from 'evergreen-ui';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+Enzyme.configure({adapter: new EnzymeAdapter() });
+
+describe('state controlled input field',() => {
+  let setV = jest.fn((value) => {return value});
+  beforeEach(() => {
+     setV.mockClear();
+     React.useState = jest.fn(() => [false, setV]);
+    React.useState.mockClear();
+  })
+  
+test('evergreen-ui checkbox onchange', () => {
+
+  const wrapper = mount(<App/>);
+// the evergreen-ui root node is a forwardref and not an input type - atleast to my knowledge
+// this is why I have to query the evergreen-ui element and the input element nested within
+  const cb = wrapper.find('[data-test="evergreen-ui-checkbox"]').find('input');;
+
+  expect(cb).not.toBe(null);
+  expect(cb.prop('checked')).toBe(false);
+  cb.simulate('change', { target: {checked: true}});
+  wrapper.update();
+  expect(wrapper.find('[data-test="evergreen-ui-checkbox"]').find('input').prop('checked')).toBe(true);
+
+  console.log(cb.debug());
+
+  expect(setV).toHaveBeenCalledWith(true);
+
 });
+
+test('regular checkbox onchange', () => {
+
+  const wrapper = mount(<App/>);
+  const cb = wrapper.find('[data-test="regular-checkbox"]');
+  console.log(cb.debug());
+  expect(cb).not.toBe(null);
+  expect(cb.prop('checked')).toBe(false);
+  // console.log(box.debug());
+  console.log(cb.debug());
+
+
+  cb.simulate('change', { target: {checked: true}});
+  wrapper.update();
+  expect(wrapper.find('[data-test="regular-checkbox"]').prop('checked')).toBe(true);
+  expect(setV).toHaveBeenCalledWith(true);
+
+});
+});
+
+
+// test('checkbox 2', () => {
+//   const setValue = jest.fn((value) => {
+//     return value;
+//   });
+//   const wrapper = mount(<App/>);
+//   console.log(wrapper.debug());
+//   const box = wrapper.find("Checkbox");
+//   console.log(box.debug());
+
+//   console.log
+
+//   box.simulate('change', {target: {value: true}})
+//   // expect(setValue).toBeCalledWith(true);
+//   expect(setValue).toHaveBeenLastCalledWith(true);
+
+
+
+// });
+
